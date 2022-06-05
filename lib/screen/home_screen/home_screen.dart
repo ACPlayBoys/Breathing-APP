3ff3d30b1f9b7d692d0ac1f651d551ea92772088
Tejas Rai <91219997+Rai09Tejas@users.noreply.github.com>
2022-06-05 19:41:55 +0530

    Update home_screen.dart

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:breathing_app/models/musicmodel.dart';
 import 'package:breathing_app/screen/home_screen/mdrawer.dart';
 import 'package:breathing_app/util/Storage.dart';
@@ -6,6 +9,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_gifimage/flutter_gifimage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeSCreen extends StatefulWidget {
   const HomeSCreen({Key? key}) : super(key: key);
@@ -15,6 +21,9 @@ class HomeSCreen extends StatefulWidget {
 }
 
 class _HomeSCreenState extends State<HomeSCreen> with TickerProviderStateMixin {
+  final _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
   final String path = "asset/images/home/";
   MusicModel m = MusicModel(
       duration: "300",
@@ -24,18 +33,36 @@ class _HomeSCreenState extends State<HomeSCreen> with TickerProviderStateMixin {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   List<MusicModel> list = [];
 
+  String currentUid = '';
+
   bool isPlaying = false;
+  bool canPlay = true;
+  bool isMute = true;
+  bool docExist = false;
+  bool checkdone = false;
+
+  int breatheTime = 0;
+  int time = 0;
+  late int playedTime;
+
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  String url =
+      'https://firebasestorage.googleapis.com/v0/b/internship-df344.appspot.com/o/breathe.mp3?alt=media&token=36329fe1-4243-46f5-ba62-cdb9a2055f55';
 
   late GifController controller;
   int speed = 1000;
   @override
   void initState() {
+    playedTime = 1;
     controller = GifController(vsync: this);
     list.add(m);
     list.add(m);
     list.add(m);
     list.add(m);
-
+    currentUid = _auth.currentUser!.uid;
+    getReportData();
+    getAudio();
     super.initState();
   }
 
@@ -57,12 +84,12 @@ class _HomeSCreenState extends State<HomeSCreen> with TickerProviderStateMixin {
               Spacer(),
               "Breathe In".text.xl3.bold.make(),
               Spacer(),
-              buildContainer(child: Icon(CupertinoIcons.volume_off).p4())
+              buildContainerOnTap(child: Icon(CupertinoIcons.volume_off).p4())
             ],
           ).pOnly(top: y / 24, bottom: y / 8).px(x / 24),
           GifImage(
             height: y / 4,
-            image: NetworkImage(Storage.gifUrl.link),
+            image: NetworkImage(Storage.gifUrl),
             controller: controller,
           ).centered().pOnly(bottom: y / 8),
           "Breathing Rate"
@@ -81,7 +108,7 @@ class _HomeSCreenState extends State<HomeSCreen> with TickerProviderStateMixin {
               ).onInkTap(() {
                 speed > 200 ? speed -= 200 : speed = 200;
                 controller.repeat(
-                    min: 0, max:Storage.gifUrl.frames, period: Duration(milliseconds: speed));
+                    min: 0, max: 3, period: Duration(milliseconds: speed));
               }).pOnly(right: x / 32),
               Container(
                 child:
@@ -89,24 +116,189 @@ class _HomeSCreenState extends State<HomeSCreen> with TickerProviderStateMixin {
               ).onInkTap(() {
                 speed < 2000 ? speed += 200 : speed = 2000;
                 controller.repeat(
-                    min: 0, max:Storage.gifUrl.frames, period: Duration(milliseconds: speed));
+                    min: 0, max: 3, period: Duration(milliseconds: speed));
               }).pOnly(left: x / 32)
             ],
           ).pOnly(bottom: y / 32),
           Container(
             child: buildContainer(
-                child: Image.asset(
-                        !isPlaying ? path + "playdeep.png" : path + "pausedeep.png")
-                    .onInkTap(() {
-              setState(() {
-                isPlaying
-                    ? controller.stop()
-                    : controller.repeat(
-                        min: 0, max:Storage.gifUrl.frames, period: Duration(milliseconds: speed));
-                isPlaying = !isPlaying;
-              });
-            }).p8()),
+                child: playedTime == 1
+                    ? canPlay
+                        ? Image.asset(!isPlaying
+                                ? path + "play.png"
+                                : path + "pause.png")
+                            .onInkTap(() {
+                            setState(() {
+                              isPlaying ? onPause() : onPlay();
+                              isPlaying = !isPlaying;
+                            });
+                          }).p8()
+                        : CircularProgressIndicator()
+                    : Image.asset(
+                            !isPlaying ? path + "play.png" : path + "pause.png")
+                        .onInkTap(() {
+                        setState(() {
+                          isPlaying ? onPause() : onPlay();
+                          isPlaying = !isPlaying;
+                        });
+                      }).p8()),
           ).centered()
         ])));
+  }
+
+  onPause() {
+    controller.stop();
+    // pauseHandler();
+    audioPlayer.pause();
+    timer();
+    print('audio Paused');
+  }
+
+  onPlay() {
+    print('depalyed playedtime $playedTime');
+    controller.repeat(min: 0, max: 9, period: Duration(milliseconds: speed));
+    // playHandler();
+
+    audioPlayer.resume();
+    setState(() {
+      canPlay = false;
+      print('depalyed caplay $canPlay');
+    });
+    Timer(Duration(seconds: 5), () {
+      setState(() {
+        canPlay = true;
+        print('depalyed caplay $canPlay');
+      });
+    });
+    timer();
+    setState(() {
+      playedTime += 1;
+    });
+    print('depalyed playedtime $playedTime');
+
+    print('audio Playing');
+  }
+
+  onMute() {
+    if (isMute) {
+      audioPlayer.setVolume(0);
+      isMute = !isMute;
+    } else {
+      audioPlayer.setVolume(1);
+      isMute = !isMute;
+    }
+  }
+
+  timer() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      print('timerStarted');
+      if (isPlaying == true) {
+        setState(() {
+          time = time + 1;
+          print(time);
+        });
+      } else {
+        print('timerStopped $time');
+        addReport();
+        timer.cancel();
+        time = 0;
+      }
+    });
+  }
+
+  getAudio() async {
+    var snap = await _firestore
+        .collection('Users')
+        .doc('15OX5wUmgcUU5r2NxpIHUdXYsZl1')
+        .get();
+    var audioType = snap['audioType'];
+    print('audioType $audioType');
+    if (audioType == 'default') {
+      setState(() {
+        url =
+            'https://firebasestorage.googleapis.com/v0/b/internship-df344.appspot.com/o/breathe.mp3?alt=media&token=36329fe1-4243-46f5-ba62-cdb9a2055f55';
+      });
+    } else if (audioType == 'userAudio') {
+      var snapshot = await _firestore
+          .collection('Users')
+          .doc('15OX5wUmgcUU5r2NxpIHUdXYsZl1')
+          .collection('audioCollection')
+          .orderBy('timeStamp', descending: false)
+          .get();
+      print('await complete');
+      print(snapshot.docs.length);
+      var reqdoc = snapshot.docs.length - 1;
+      print(snapshot.docs[reqdoc].data()['link']);
+      setState(() {
+        if (url != null) {
+          url = snapshot.docs[reqdoc].data()['link'];
+          print('got url ');
+        } else {
+          url =
+              'https://firebasestorage.googleapis.com/v0/b/internship-df344.appspot.com/o/breathe.mp3?alt=media&token=36329fe1-4243-46f5-ba62-cdb9a2055f55';
+          print('got url ');
+        }
+        print('url $url');
+      });
+    }
+    audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+    audioPlayer.setUrl(url);
+    print('audioSet');
+  }
+
+  Widget buildContainerOnTap({child}) {
+    return ClipOval(
+      clipBehavior: Clip.antiAlias,
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(2.0), //width of the border
+          child: ClipOval(
+            clipBehavior: Clip.antiAlias,
+            child: GestureDetector(
+              onTap: () => onMute(),
+              child: Container(
+                // this height forces the container to be a circle
+                child: child,
+
+                decoration: kInnerDecoration,
+              ),
+            ),
+          ),
+        ),
+        decoration: kGradientBoxDecoration,
+      ),
+    );
+  }
+
+  getReportData() async {
+    print(' got report data');
+    var data = await _firestore.collection('report').doc(currentUid).get();
+    setState(() {
+      breatheTime = data['todayTotal'];
+      print('breatheTime $breatheTime');
+    });
+  }
+
+  addReport() {
+    print('adding report');
+    String actDoc = currentUid +
+        DateFormat.y().format(DateTime.now()) +
+        DateFormat.M().format(DateTime.now()) +
+        DateFormat.d().format(DateTime.now());
+
+    breatheTime = breatheTime + time;
+    _firestore
+        .collection('report')
+        .doc(currentUid)
+        .collection('everydayActivity')
+        .doc(actDoc)
+        .set({
+      'breatheTime': breatheTime,
+      'timeStamp': DateTime.now(),
+      'date': DateFormat.yMd().format(DateTime.now())
+    }, SetOptions(merge: true));
+    _firestore.collection('report').doc(currentUid).set({
+      'todayTotal': breatheTime,
+    }, SetOptions(merge: true));
   }
 }
