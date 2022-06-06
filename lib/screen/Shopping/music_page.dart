fb5f217ec7d504bf0f7d0a6d945303ea63085924
Tejas Rai <91219997+Rai09Tejas@users.noreply.github.com>
2022-06-06 19:29:53 +0530

    Update music_page.dart

--- lib/screen/Shopping/music_page.dart
+++ lib/screen/Shopping/music_page.dart
@@ -1,12 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:audioplayers/audioplayers.dart';
-import 'package:breathing_app/util/Storage.dart';
-import 'package:breathing_app/util/routes.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'package:velocity_x/velocity_x.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:breathing_app/models/musicmodel.dart';
 import 'package:breathing_app/screen/home_screen/mdrawer.dart';
 import 'package:breathing_app/util/constants.dart';
@@ -22,27 +19,67 @@ class MusicScreen extends StatefulWidget {
 }
 
 class _MusicScreenState extends State<MusicScreen> {
+  late Razorpay razorpay;
+
   final String path = "asset/images/shopping/";
   late MusicModel m;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  bool isplaying = false;
-  bool playing = false;
-  AudioPlayer player = AudioPlayer(mode: PlayerMode.MEDIA_PLAYER);
-  AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.MEDIA_PLAYER);
+
+  List<MusicModel> list = [];
+
   @override
   void initState() {
-    m = widget.m;
+    razorpay = new Razorpay();
 
-    audioPlayer.setUrl(widget.m.link);
+    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerSucess);
+    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerError);
+    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExtWallet);
+
+    m = widget.m;
+    list.add(m);
+    list.add(m);
+    list.add(m);
+    list.add(m);
 
     super.initState();
-    addToRecent();
+  }
+
+  void openCheckout() {
+    var options = {
+      "key": "rzp_test_ERMWIEMeuGzC4C",
+      "amount": 200, //m.price (add price to music model)
+      "name": "Breathe App",
+      "description": "Payment for ${m.name}",
+      "prefill": {"contact": "2323232323", "email": "shdjsdh@gmail.com"},
+      "external": {
+        "wallets": ["paytm"]
+      }
+    };
+
+    try {
+      razorpay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  handlerSucess() {
+    showToast(context, 'Payment Sucessful');
+  }
+
+  handlerError() {
+    showToast(context, 'Payment Failed');
+  }
+
+  handlerExtWallet() {
+    showToast(context, 'External Wallet');
   }
 
   @override
   void dispose() {
-    audioPlayer.dispose(); // TODO: implement dispose
+    // TODO: implement dispose
     super.dispose();
+    razorpay.clear();
   }
 
   @override
@@ -53,163 +90,120 @@ class _MusicScreenState extends State<MusicScreen> {
         key: _scaffoldKey,
         drawer: MDrawer(),
         body: SafeArea(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Row(
-            children: [
-              Image.asset(path + "back.png").onInkTap(() {
-                Navigator.of(context).pop();
-              }),
-              Spacer(),
-              Image.asset(path + "search.png").onInkTap(() {
-                Navigator.of(context).pop();
-              }),
-            ],
-          ).pOnly(top: y / 25, bottom: y / 24).px(x / 24),
-          buildContainer(child: Image.network(m.image).p8()).centered(),
-          m.name.text.xl2.bold.makeCentered(),
-          "${m.duration} Mins".text.lg.makeCentered(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              buildContainer(
-                  child: Image.asset(!isplaying
-                          ? path + "playdeep.png"
-                          : path + "pausedeep.png")
-                      .onInkTap(() {
-                isplaying = !isplaying;
-                if (isplaying) {
-                  audioPlayer.resume();
-                } else {
-                  audioPlayer.pause();
-                }
-                setState(() {});
-              }).p8()),
-            ],
-          ),
-          "Similar Tracks".text.bold.color(borderColor).make(),
-          ListView.separated(
-                  itemBuilder: ((context, index) {
-                    MusicModel m = Storage.audios.value[index];
-                    return Container(
-                      child: Row(
-                        children: [
-                          buildContainer(
-                            child: Image.network(
-                              m.image,
-                              width: 60,
-                            ).p4(),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              m.name.text.extraBold.make(),
-                              m.duration.text.extraBold.make(),
-                            ],
-                          ).px(x / 32),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Image.asset(!playing
-                                      ? (path + "play.png")
-                                      : (path + "pause.png"))
-                                  .onInkTap(() {
-                                playing = !playing;
-                                // setState(() {});
-                                if (!playing)
-                                  player.play(m.link);
-                                else
-                                  player.pause();
-                              }),
-                            ],
-                          ).expand()
-                        ],
-                      ),
-                    )
-                        .onInkTap(() {
-                          //audioPlayer.dispose();
-                          Navigator.of(context)
-                              .pushReplacement(Routes.createMusicRoute(m));
-                        })
-                        .px(x / 16)
-                        .py(y / 128);
+            child: Stack(
+          children: [
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                children: [
+                  Image.asset(path + "back.png").onInkTap(() {
+                    Navigator.of(context).pop();
                   }),
-                  separatorBuilder: (context, index) {
-                    return Divider(thickness: 1).px16();
-                  },
-                  itemCount: Storage.audios.value.length)
-              .expand(),
-          Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                height: y / 8,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: x / 2.5,
-                      height: y / 16,
-                      child:
-                          "Add to WishList".text.lg.bold.makeCentered().px16(),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue),
-                          borderRadius: BorderRadius.circular(y / 16)),
-                    ).onInkTap(() {
-                      addToWishlist(context);
-                    }),
-                    Container(
-                      width: x / 2.5,
-                      height: y / 16,
-                      child: "Buy Now".text.lg.bold.makeCentered().px16(),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue),
-                          borderRadius: BorderRadius.circular(y / 16)),
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: borderColor.withOpacity(0.1),
-                      offset: Offset(0.0, -19.0), //(x,
-                      blurRadius: 50.0,
-                      spreadRadius: 2,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )
-        ])));
-  }
-
-  addToRecent() {
-    FirebaseFirestore.instance
-        .collection("Users")
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .collection("recents")
-        .doc(m.name)
-        .set(m.toMap())
-        .then((value) => FirebaseFirestore.instance
-            .collection("Users")
-            .doc(FirebaseAuth.instance.currentUser!.uid)
-            .collection("recents")
-            .doc(m.name)
-            .set({"time": DateTime.now().toIso8601String()},
-                SetOptions(merge: true)));
-  }
-
-  addToWishlist(BuildContext context) {
-    FirebaseFirestore.instance
-        .collection("Users")
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .collection("Wishlist")
-        .doc(m.name)
-        .set(m.toMap())
-        .then((value) => showToast(context, "Added to WishList"));
+                  Spacer(),
+                  Image.asset(path + "search.png").onInkTap(() {
+                    Navigator.of(context).pop();
+                  }),
+                ],
+              ).pOnly(top: y / 25, bottom: y / 30).px(x / 24),
+              buildContainer(child: Image.asset(m.image).p8()).centered(),
+              m.name.text.xl2.bold.makeCentered(),
+              "${m.duration} Mins".text.lg.makeCentered(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildContainer(
+                      child: Image.asset(path + "playdeep.png").p8()),
+                ],
+              ),
+              "Similar Tracks".text.bold.color(borderColor).make(),
+              ListView.separated(
+                      itemBuilder: ((context, index) {
+                        MusicModel m = list[index];
+                        return Container(
+                          child: Row(
+                            children: [
+                              buildContainer(
+                                child: Image.asset(
+                                  m.image,
+                                  width: 60,
+                                ).p4(),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  m.name.text.extraBold.make(),
+                                  m.duration.text.extraBold.make(),
+                                ],
+                              ).px(x / 32),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Image.asset(
+                                      "asset/images/shopping/" + "play.png"),
+                                ],
+                              ).expand()
+                            ],
+                          ),
+                        ).px(x / 16).py(y / 128);
+                      }),
+                      separatorBuilder: (context, index) {
+                        return Divider(thickness: 1).px16();
+                      },
+                      itemCount: list.length)
+                  .expand(),
+            ]),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: y / 8,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: x / 2.5,
+                        height: y / 16,
+                        child: "Add to WishList"
+                            .text
+                            .lg
+                            .bold
+                            .makeCentered()
+                            .px16(),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            borderRadius: BorderRadius.circular(y / 16)),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          openCheckout();
+                        },
+                        child: Container(
+                          width: x / 2.5,
+                          height: y / 16,
+                          child: "Buy Now".text.lg.bold.makeCentered().px16(),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue),
+                              borderRadius: BorderRadius.circular(y / 16)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: borderColor.withOpacity(0.1),
+                        offset: Offset(0.0, -19.0), //(x,
+                        blurRadius: 50.0,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        )));
   }
 }
